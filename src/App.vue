2aabd44fd0a51d1def8d4de19034b180a118a624
Henrique de Castilhos <henriquedecastilhos@gmail.com>
2019-03-08 17:59:39 -0300

    Calculadora

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,68 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Calculadora Vue.Js</h1>
+
+    <div>
+      <input v-model="valorA" type="number" placeholder="Valor A">
+      <input v-model="valorB" type="number" placeholder="Valor B">
+    </div>
+
+    <div>
+      <button @click="somar()">+</button>
+      <button @click="subtrair()">-</button>
+      <button @click="multiplicar()">*</button>
+      <button @click="dividir()">/</button>
+    </div>
+
+    <div v-if="resultado != null">
+      <h3>
+        Resultado: {{ resultado }}
+      </h3>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
+
+  data() {
+    return {
+      valorA: null,
+      valorB: null,
+      resultado: null
+    }
+  },
+
+  methods: {
+    somar () {
+      if (this.verificaValores()) {
+        this.resultado = parseInt(this.valorA) + parseInt(this.valorB)
+      }
+    },
+    subtrair () {
+      if (this.verificaValores()) {
+        this.resultado = parseInt(this.valorA) - parseInt(this.valorB)
+      }
+    },
+    multiplicar () {
+      if (this.verificaValores()) {
+        this.resultado = parseInt(this.valorA) * parseInt(this.valorB)
+      }
+    },
+    dividir () {
+      if (this.verificaValores()) {
+        this.resultado = parseInt(this.valorA) / parseInt(this.valorB)
+      }
+    },
+    verificaValores () {
+      if (this.valorA && this.valorB) {
+        return true
+      }
+
+      return false
+    }
+  },
 }
 </script>
 
@@ -24,5 +74,14 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+input, button {
+  margin-left: 5px;
+  margin-right: 5px;
+}
+
+div {
+  margin: 10px;
 }
 </style>
